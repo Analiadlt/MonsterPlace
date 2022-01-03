73649e3e79404a7398736cd8b18d3a59f47e3c5e
Jose Limardo <86510147+jhlimardo@users.noreply.github.com>
2022-01-03 10:24:45 -0500

    Revert "Update App.js"

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,6 @@ function App() {
     <div className="App">
       <h1>Henry Pokemon</h1>
       <h1>Hola</h1>
- de-prueba
-      <h1>prueba</h1>
-      <h1>Prueba reglas</h1>
- main
     </div>
   );
 }
