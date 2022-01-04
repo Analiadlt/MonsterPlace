b02432bcb2f0f8bd814e42e91f496aae4d3df93c
alvarovega99 <vegaa8238@gmail.com>
2022-01-04 18:30:14 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+
+import React from 'react'; 
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/index.js'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store ={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
