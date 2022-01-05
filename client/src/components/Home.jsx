e69b42fcce2eac476c701451e11a032094208d6f
alvarovega99 <vegaa8238@gmail.com>
2022-01-05 13:58:56 -0300

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,16 @@
 import React  from 'react';
-
+import Nav from './Nav';
+import Slider from './Slider'
 export default function Home() {
 
     return (
+        <div>
+            <Nav/>
+            <div className='slider-home' >
+            <Slider/>
+            </div>
 
-        <div className="home">
-            <h1>Hola</h1>
+
         </div>
     )
 }
