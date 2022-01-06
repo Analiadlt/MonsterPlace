7c94f119da9deaffadeca0b6440bd8a5e358d0c8
Reyvenus <lu14393@gmail.com>
2022-01-06 01:31:11 -0300

    end-point POST corregido

--- api/src/routes/POST/PostUser.js
+++ api/src/routes/POST/PostUser.js
@@ -5,54 +5,26 @@ const server = express()
 
 server.post("/", async (req, res) => {
     const { email, firstName, lastName, nickName, dateBirth, password, token } = req.body
-    console.log("DATOS", dateBirth)
 
-    // try {
-    //     if (!email || !userName || !nickName || !dateBirth || !password || !token) res.send(" Debe completar todos los campos")
-
-    //     else {
-    //         const [user, created] = await User.FindOrCreate({
-    //             where: { email: email },
-    //             defaults: {
-    //                 userName: userName,
-    //                 nickName: nickName,
-    //                 dateBirth: dateBirth,
-    //                 password: password,
-    //                 token: token
-    //             }
-    //         })
-    //         console.log("USUARIO", user)
-    //         res.send("creado")
-    //     }
-
-    // }
-    // catch (error) {
-    //     res.send("Ups...!!!existe un error")
-    // }
-    
-    
     try {
-        let user;
-        user = await User.findOne({ where: { email: email }, })
-        if (!user) {
-            user = await User.create({
-                email: email,
-                firstName: firstName,
-                lastName: lastName,
-                nickName: nickName,
-                dateBirth: dateBirth,
-                password: password,
-                token: token
-            })
+        if (!email || !firstName || !lastName || !nickName || !dateBirth || !password || !token) res.send(" Debe completar todos los campos")
+        else {
+            let user;
+            user = await User.findOne({ where: { email: email }, })
+            if (!user) {
+                user = await User.create({
+                    email: email,
+                    firstName: firstName,
+                    lastName: lastName,
+                    nickName: nickName,
+                    dateBirth: dateBirth,
+                    password: password,
+                    token: token
+                })
+                res.send("Usuario creado con exito").json(user)
+            }
+            else res.send("El usuario ya existe")
         }
-        
-        for (const country of countries) {
-            country.addActivity(activity)
-        }
-        
-        // const union = await activity.addCountries(countryID)
-        // res.send(union)
-        res.send(activity)
     }
     catch (error) {
         res.send("Ups...!!!existe un error")
