520b6a29fff5a815f829a79ecc6e13499e2346ec
Jose <86510147+jhlimardo@users.noreply.github.com>
2022-01-05 19:59:27 -0500

    Formulario de Registro
    Se modifico:
    Package.jsno
    App.js
    FormRegistro.js
    _formulario.scss
    _index.scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Home from './components/Home'
-
-
-
+import FormRegistro from './components/FormRegistro'
 import './sass/app.scss'
+
 function App() {
 
   return (
     <div className="App">
       <Route path="/Home" component={Home} />
+      <div className="contenedor">
+      <Route path="/Registro" component={FormRegistro} />
+      </div>
     </div>
   );
 }
