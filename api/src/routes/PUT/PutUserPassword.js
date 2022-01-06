84f7af77fa4dac962d302101cf259bb4e2878af3
Gustavo Montero <79276560+gamontero@users.noreply.github.com>
2022-01-06 00:53:59 -0300

    PUT User Password Route
    cree la ruta de modificar el password en base de datos, falta validaciones y pruebas. Me da estatus 200 al probarla sin haber creado usuario.

---
+++ api/src/routes/PUT/PutUserPassword.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const { User } = require("../../db");
+const router = Router();
+
+router.put("/putUserPassword", async (req, res) => {
+    
+  try {
+    const { email, password } = req.body;
+    const updatePassword = await User.update({ password },
+        {where: {email: email}} );
+
+    return res.status(200).send('Password Updated Successfully');
+
+  } catch (e) {
+    res.status(404).send("Cant access DB. GET CARDS PROBLEMS");
+  }
+});
+
+module.exports = router;
