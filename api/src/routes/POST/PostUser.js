2f7f750baadbcef8317e466cf3d36a1e869fb602
Reyvenus <lu14393@gmail.com>
2022-01-06 00:59:51 -0300

    arreglando post

--- api/src/routes/POST/PostUser.js
+++ api/src/routes/POST/PostUser.js
@@ -4,29 +4,60 @@ const { User } = require("../../db")
 const server = express()
 
 server.post("/", async (req, res) => {
-    const { email, userName, nickName, dateBirth, password, token } = req.body
+    const { email, firstName, lastName, nickName, dateBirth, password, token } = req.body
     console.log("DATOS", dateBirth)
 
-    try {
-        if (!email || !userName || !nickName || !dateBirth || !password || !token) res.send(" debe completar todos los campos")
+    // try {
+    //     if (!email || !userName || !nickName || !dateBirth || !password || !token) res.send(" Debe completar todos los campos")
 
-        else {
-            const userCreate = await User.create({
+    //     else {
+    //         const [user, created] = await User.FindOrCreate({
+    //             where: { email: email },
+    //             defaults: {
+    //                 userName: userName,
+    //                 nickName: nickName,
+    //                 dateBirth: dateBirth,
+    //                 password: password,
+    //                 token: token
+    //             }
+    //         })
+    //         console.log("USUARIO", user)
+    //         res.send("creado")
+    //     }
+
+    // }
+    // catch (error) {
+    //     res.send("Ups...!!!existe un error")
+    // }
+    
+    
+    try {
+        let user;
+        user = await User.findOne({ where: { email: email }, })
+        if (!user) {
+            user = await User.create({
                 email: email,
-                userName: userName,
+                firstName: firstName,
+                lastName: lastName,
                 nickName: nickName,
                 dateBirth: dateBirth,
                 password: password,
                 token: token
             })
-            console.log("CREADO", userCreate)
-            res.json(userCreate)
         }
-
+        
+        for (const country of countries) {
+            country.addActivity(activity)
+        }
+        
+        // const union = await activity.addCountries(countryID)
+        // res.send(union)
+        res.send(activity)
     }
     catch (error) {
-        res.send(error)
+        res.send("Ups...!!!existe un error")
     }
 })
+
 
 module.exports = server
