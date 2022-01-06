eb32de9956d419e52db40e7516a10805bf2143d6
Reyvenus <84596076+Reyvenus@users.noreply.github.com>
2022-01-06 01:55:56 -0300

    Post-user modificado (#34)
    * agregado endpoint post-user
    
    * arreglando post
    
    * end-point POST corregido
    
    Co-authored-by: Klaus Amapane <83513050+klauss3895@users.noreply.github.com>

--- api/src/routes/POST/PostUser.js
+++ api/src/routes/POST/PostUser.js
@@ -4,29 +4,33 @@ const { User } = require("../../db")
 const server = express()
 
 server.post("/", async (req, res) => {
-    const { email, userName, nickName, dateBirth, password, token } = req.body
-    console.log("DATOS", dateBirth)
+
+    const { email, firstName, lastName, nickName, dateBirth, password, token } = req.body
 
     try {
-        if (!email || !userName || !nickName || !dateBirth || !password || !token) res.send(" debe completar todos los campos")
-
+        if (!email || !firstName || !lastName || !nickName || !dateBirth || !password || !token) res.send(" Debe completar todos los campos")
         else {
-            const userCreate = await User.create({
-                email: email,
-                userName: userName,
-                nickName: nickName,
-                dateBirth: dateBirth,
-                password: password,
-                token: token
-            })
-            console.log("CREADO", userCreate)
-            res.json(userCreate)
+            let user;
+            user = await User.findOne({ where: { email: email }, })
+            if (!user) {
+                user = await User.create({
+                    email: email,
+                    firstName: firstName,
+                    lastName: lastName,
+                    nickName: nickName,
+                    dateBirth: dateBirth,
+                    password: password,
+                    token: token
+                })
+                res.send("Usuario creado con exito").json(user)
+            }
+            else res.send("El usuario ya existe")
         }
-
     }
     catch (error) {
-        res.send(error)
+        res.send("Ups...!!!existe un error")
     }
 })
+
 
 module.exports = server
