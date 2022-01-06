f430f1f914c6331703b06cfdc849bc7b605b9fbf
alvarovega99 <vegaa8238@gmail.com>
2022-01-06 19:45:17 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,16 @@ import { Route } from 'react-router-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Home from './components/Home'
 import FormRegistro from './components/FormRegistro'
+import Tienda from './components/Tienda';
 import './sass/app.scss'
 
 function App() {
 
   return (
     <div className="App">
-
-     
-
       <Route  exact path="/" component={Home} />
+      <Route  exact path="/Tienda" component={Tienda} />
       <div className="contenedoor">
-
       <Route path="/Registro" component={FormRegistro} />
       </div>
     </div>
