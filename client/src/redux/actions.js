1b9492621629d9551d593df0d89630028b2b115e
Ludwig Driet <83729472+LudwigDriet@users.noreply.github.com>
2022-01-07 15:26:44 -0300

    Update actions.js

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const CAMBIAR_FONDO = "CAMBIAR_FONDO"
 export const ADD_USER = "ADD_USER"
 export const GET_USER = "GET_USER"
+export const LOGIN_USER = "LOGIN_USER"
 export const LOADING = "LOADING"
 export const ADD_CART = "ADD_CART"
 
@@ -21,6 +22,23 @@ export function addUser(payload) {
         var json = await axios.post(`http://localhost:3001/users`, payload);
         return dispatch({
           type: ADD_USER, 
+          payload: json,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+
+}
+
+export function loginUser(payload) {
+  console.log("Datos para posteo", payload)
+    return async (dispatch) => {
+      try {
+        var json = await axios.post(`http://localhost:3001/users`, payload);
+        return dispatch({
+          type: LOGIN_USER, 
           payload: json,
         });
       } catch (error) {
