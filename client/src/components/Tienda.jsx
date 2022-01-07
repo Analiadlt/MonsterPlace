d31190440369a7fd937db0932821165bcaa00287
alvarovega99 <vegaa8238@gmail.com>
2022-01-06 21:14:11 -0300

    Update Tienda.jsx

--- client/src/components/Tienda.jsx
+++ client/src/components/Tienda.jsx
@@ -5,11 +5,10 @@ import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 export default function Tienda(){
     return(
         <div>
-            <Nav/>
             <div className="contenedor">
                 <div className="titulo-tienda">
                     <h1>Tienda</h1>
-                    <h1 className="carrito"><ShoppingBasketIcon/></h1>
+                    <span className="carrito"><ShoppingBasketIcon fontSize="3rem"/></span>
                 </div>
             </div>
         </div>
