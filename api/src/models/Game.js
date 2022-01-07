df1386c60be185fb642ca7785fb503acdceb0355
Analiadlt <87041305+Analiadlt@users.noreply.github.com>
2022-01-07 19:55:22 -0300

    Update Game.js
    Agregué:
    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true},

--- api/src/models/Game.js
+++ api/src/models/Game.js
@@ -4,6 +4,7 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('game', {
+    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true},
     idPlayer1: { type: DataTypes.INTEGER }, 
     idPlayer2: { type: DataTypes.INTEGER }, 
     result: { type: DataTypes.ENUM('player1', 'player2', 'tie', ''), defaultValue: '' },
