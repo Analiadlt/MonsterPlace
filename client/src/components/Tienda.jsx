0f6a9cc54654b849865f00b729a4fc2bde031695
alvarovega99 <vegaa8238@gmail.com>
2022-01-07 00:11:39 -0300

    Update Tienda.jsx

--- client/src/components/Tienda.jsx
+++ client/src/components/Tienda.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import Card from '../components/Card'
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 export default function Tienda(){
     const dragones = useSelector(state => state.dragonesbd)
     const carrito = useSelector(state => state.carrito)
@@ -11,7 +12,9 @@ export default function Tienda(){
             <div className="contenedor-tienda">
                 <div className="titulo-tienda">
                     <h1>Tienda</h1>
-                    <h3 className="carrito"><span className="carrito-num">{carrito.length}</span><ShoppingBasketIcon fontSize="5rem"/></h3>
+                    <Link to='/Carrito'>
+                        <h3 className="carrito"><span className="carrito-num">{carrito.length}</span><ShoppingBasketIcon fontSize="5rem"/></h3>
+                    </Link>
                 </div>
 
                 <div className="contenedor-tajetas">
