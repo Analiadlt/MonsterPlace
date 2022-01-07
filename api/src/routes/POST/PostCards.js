6f33b19ab871d5df93412fcda1813f09c20e58c5
klauss3895 <klauss3895@gmail.com>
2022-01-07 14:52:11 -0300

    ruta postCard

--- api/src/routes/POST/PostCards.js
+++ api/src/routes/POST/PostCards.js
@@ -1,15 +1,12 @@
 const {Router} = require ("express");
-const {User,Card} = require("../../db");
+const {Card} = require("../../db");
 
 
 const router = Router();
 
-
-
-
 router.post("/", async (req,res)=>{
     let {name,attack,defense,img,state,condition,sellPrice}=req.body;
-    console.log(req.body)
+
     try {
 
         const [newCard,create] = await Card.findOrCreate({where:{name:name},defaults:{
@@ -20,12 +17,9 @@ router.post("/", async (req,res)=>{
             condition,
             sellPrice
         }});
-        console.log("NewCard",newCard)
     
-        
-        res.json(newCard);
+        res.send(newCard);
     } catch (error) {
-        console.log(error)
         res.send ("Error al crear carta")
     }
 
