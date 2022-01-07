e27fd9b3147284ec011048dc444f31c73b9cc147
alvarovega99 <vegaa8238@gmail.com>
2022-01-07 00:20:01 -0300

    Update Carrito.jsx

--- client/src/components/Carrito.jsx
+++ client/src/components/Carrito.jsx
@@ -4,6 +4,13 @@ import { useSelector } from "react-redux";
 
 export default function(){
     const carrito = useSelector(state=> state.carrito)
+    function sumarCarrito(carrito){
+        let total = 0;
+        for(let i = 0 ; i < carrito.length; i++){
+            total += carrito[i].price;
+        }
+        return total;
+    }
     return(
         <div>
             <h1>Carrito</h1>
@@ -12,6 +19,10 @@ export default function(){
                     <Card name={dragon.name} atack={dragon.atack} defense={dragon.defense}  img={dragon.img} price={dragon.price} />
                 )
             }
+            <div>
+                <h1>Total</h1>
+                <h3>{sumarCarrito(carrito)}</h3>
+            </div>
         </div>
     )
 
