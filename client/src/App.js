d3bd9ad023b6335526e57e869fea830c11785169
Alvaro Vega <64806626+alvarovega99@users.noreply.github.com>
2022-01-07 00:21:33 -0300

    Alvaro parch 9 (#52)
    * Update App.js
    
    * Update Card.jsx
    
    * Create Carrito.jsx
    
    * Update Tienda.jsx
    
    * Update reducer.js
    
    * Update Carrito.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,13 @@ import FormRegistro from './components/FormRegistro'
 import Tienda from './components/Tienda';
 import './sass/app.scss'
 import Nav from './components/Nav';
-
+import Carrito from './components/Carrito';
 function App() {
 
   return (
     <div className="App">
       <Route path="/" component={Nav} />
+      <Route path="/Carrito" component={Carrito} />
       <Route  exact path="/" component={Home} />
       <Route  exact path="/Tienda" component={Tienda} />
       <div className="contenedoor">
