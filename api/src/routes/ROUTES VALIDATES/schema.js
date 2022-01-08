192270bec102f5498d9e065f2fbf3b504a9b965a
klauss3895 <klauss3895@gmail.com>
2022-01-08 02:10:04 -0300

    validaciones

---
+++ api/src/routes/ROUTES VALIDATES/schema.js
@@ -0,0 +1,41 @@
+const Joi = require("@hapi/joi");
+
+module.exports = {
+    postCardSchema:Joi.object({
+        name:Joi.string().required(),
+        attack:Joi.number().integer().required(),
+        defense:Joi.number().integer().required(),
+        img:Joi.string().required(),
+        state:Joi.string().required(),
+        type:Joi.string().required(),
+        sellPrice:Joi.number().required(),
+        sellCount:Joi.number().integer().required()  
+    }),
+
+    postUserSchema:Joi.object({
+        email:Joi.string().email().required(),
+        firstName:Joi.string().required(),
+        lastName:Joi.string().required(),
+        nickName:Joi.string().required(),
+        dateBirth:Joi.date().iso().required(),
+        password:Joi.string().required()
+    }),
+
+    postLoginSchema:Joi.object({
+        email:Joi.string().email().required(),
+        password:Joi.string().required()
+    }),
+
+    putUserInformationSchema:Joi.object({
+        email:Joi.string().email().required(),
+        firstName:Joi.string().required(),
+        lastName:Joi.string().required(),
+        nickName:Joi.string().required(),
+        dateBirth:Joi.string().required(),
+    }),
+
+    putUserPasswordSchema:Joi.object({
+        email:Joi.string().email().required(),
+        password:Joi.string().required()
+    })
+}
