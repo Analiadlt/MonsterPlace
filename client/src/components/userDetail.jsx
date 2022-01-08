cbe53d70a038518e6488da5ff50852c8b4afb799
Ludwig Driet <83729472+LudwigDriet@users.noreply.github.com>
2022-01-08 16:10:47 -0300

    perfil

--- client/src/components/userDetail.jsx
+++ client/src/components/userDetail.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams} from "react-router-dom";
+import Nav from './Nav';
 import { 
     getById,
  } from "../redux/actions.js";
+ 
 
 
 export default function Detalle() {
@@ -12,15 +14,9 @@ export default function Detalle() {
   const [uId, ] = useState(id);
  // const aux = "840d9150-0da9-4c7b-a898-8d15e0d1827d"
 
-
-  
-
-
-console.log("Id y uId", id);
-
-  useEffect(() => {
+	useEffect(() => {
     dispatch(getById(uId));
-	}, [dispatch,uId]);
+	}, [uId]);
 
 const detail = useSelector((state) => state.userDetail);
 
@@ -29,32 +25,40 @@ console.log("detalle desde detail", detail)
 
     
   return (
+	  <div>
+		 <Nav />
+	  <div style={{display:'flex',alignItems:'center', justifyContent:'center'}}>
     <div className="detail-content">
-		<div className="detail-titulo">
-			<h1 style={{color: 'white'}}>User Profile</h1>
-		</div>
-		<div className="detail-block">
-		<div>
-			<h4> Nombre: </h4>
+		
+			<h2 >Perfil De Usuario</h2>
+		
+		
+		<div className="usuario-box">
+				
+			<h3> Nombre: </h3>
 			<p> {detail.firstName} </p>
+				
 		</div>
-		<div>
-			<h4> Apellido: </h4>
+
+		<div className="usuario-box">
+			<h3> Apellido: </h3>
 			<p> {detail.lastName} </p>
 		</div>
-		<div>
-			<h4> Email: </h4>
+		<div className="usuario-box">
+			<h3> Email: </h3>
 			<p> {detail.email} </p>
 		</div>
-		<div>
-			<h4> NickName: </h4>
+		<div className="usuario-box">
+			<h3> NickName: </h3>
 			<p> {detail.nickName} </p>
 		</div>
-		<div>
-			<h4> Fecha de Nacimiento: </h4>
+		{/* <div className="usuario-box">
+			<h3> Fecha de Nacimiento: </h3>
 			<p> {detail.dateBirth} </p>
-		</div>		
-        </div>
+		</div>		 */}
+        
     </div>
+	</div>
+	</div>
   );
 }
