fc3a884ee93a68e1d41e90c246a951eb9beede21
alvarovega99 <vegaa8238@gmail.com>
2022-01-09 00:54:54 -0300

    Update Destacadas.jsx

--- client/src/components/Destacadas.jsx
+++ client/src/components/Destacadas.jsx
@@ -7,6 +7,7 @@ import Car from './Car'
 export default function Destacadas() {
     const loading = useSelector(state => state.loading)
     const cartasbd = useSelector(state => state.dragonesbd)
+    const destacadas = cartasbd.slice(1,4)
     const dispatch = useDispatch()
     useEffect(() => {
         if (!cartasbd.length && !loading.loading) {
@@ -16,13 +17,17 @@ export default function Destacadas() {
 
     return(
         <div className="destacadas">
+            {loading.loading ? <h1>cargando...</h1>:
             <div className="contenedor-destacadas">
                 <h2>Destacadas:</h2>
                 <div>
+
+
                     <Car/>
                 </div>
                 
             </div>
+}
         </div>
     )
 
