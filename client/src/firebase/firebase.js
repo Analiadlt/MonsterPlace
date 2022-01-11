3eaaef7e3ba9d16ee39e048703ec3f5045b99cbc
Analiadlt <87041305+Analiadlt@users.noreply.github.com>
2022-01-11 19:06:38 -0300

    variables de entorno para firebase.js
    creación de archivo .env

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -3,20 +3,23 @@ import "firebase/compat/storage"
 import "firebase/compat/firestore"
 import 'firebase/compat/auth';
 
+require('dotenv').config()
+;
 // If you're not using Code Sandbox, never hard-code the keys! Add them in your .env file and link them here
 // var firebaseConfig =
 export const app = firebase.initializeApp ({
-    "apiKey": "AIzaSyDNDj9OMqE-59GHcP6qeT20HAONCYqfgtc",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 
-    "authDomain": "proyecto-final-47802.firebaseapp.com",
-  
-    "projectId": "proyecto-final-47802",
-  
-    "storageBucket": "proyecto-final-47802.appspot.com",
-  
-    "messagingSenderId": "34793343389",
-  
-    "appId": "1:34793343389:web:425a0fa3223d941c648774"
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   
 });
 // const firebaseApp = firebase.initializeApp(firebaseConfig)
+
