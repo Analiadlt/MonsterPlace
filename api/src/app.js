ae14c1ac18e0104d4a07daaa419553203d7b9107
Ludwig Driet <83729472+LudwigDriet@users.noreply.github.com>
2022-01-11 23:03:37 -0300

    Socket (#122)
    * Update _formulario.scss
    
    * Update reducer.js
    
    * Update actions.js
    
    * Update ProfileHome.jsx
    
    * Update FormRegistro.jsx
    
    * Create ForLogin.jsx
    
    * Update App.js
    
    * Update FormRegistro.jsx
    
    * socket

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,20 @@
-const express = require('express');
+var path = require('path');
+var app = require('express');
+var server = app()
+var http = require('http').createServer(server);
+
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+////
+server.get('/', (req, res) => {
+  console.error('express connection');
+  res.sendFile(path.join(__dirname, 'si.html'));
+});
 
-require('./db.js');
 
-const server = express();
+require('./db');
 
 server.name = 'API';
 
@@ -15,7 +23,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -24,12 +32,5 @@ server.use((req, res, next) => {
 
 server.use('/', routes);
 
-// Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
 
-module.exports = server;
+module.exports = http;
