022e465912fa4bdf62d998f845e452230ade874b
Gustavo Montero <79276560+gamontero@users.noreply.github.com>
2022-01-13 00:00:44 -0300

    Incorporar componentes nuevos a las rutas del front

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import Home from './components/Home'
 import FormRegistro from './components/FormRegistro'
 import Tienda from './components/Tienda';
 import './sass/app.scss'
-import Nav from './components/Nav';
+import TiendaNFT from './components/TiendaNFT';
+import crearNFT from './components/CrearNFT';
+import tableroNFT from './components/TableroNFT';
+import misNFT from './components/MisNFT';
 import Carrito from './components/Carrito';
 import ForLogin from './components/ForLogin';
 import PassReset from './components/PassReset';
-
 import userDetail from './components/userDetail';
 import MyPage from './firebase/storage/MyPage';
 function App() {
@@ -20,6 +22,10 @@ function App() {
       <Route path="/Carrito" component={Carrito} />
       <Route  exact path="/" component={Home} />
       <Route  exact path="/Tienda" component={Tienda} />
+      <Route  exact path="/TiendaNFT" component={TiendaNFT} />
+      <Route  exact path="/CrearNFT" component={crearNFT} />
+      <Route  exact path="/TableroNFT" component={tableroNFT} />
+      <Route  exact path="/MisNFT" component={misNFT} />
       <Route path="/Registro" component={FormRegistro} />
       <Route path="/Login" component={ForLogin} />
       <Route path="/PassReset" component={PassReset} />
