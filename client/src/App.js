5a0c38668f07305d083a6329fee5292eaea2b295
Ludwig Driet <83729472+LudwigDriet@users.noreply.github.com>
2022-01-13 15:05:07 -0300

    sockect

--- client/src/App.js
+++ client/src/App.js
@@ -29,11 +29,11 @@ function App() {
 
     if (app) {
         app.auth().onAuthStateChanged((authUser) => {
-          console.log('authUser',authUser.email)
+          console.log('authUser',logueado.length)
           if (authUser && logueado.length >4) {
             dispatch(getUserLogin(authUser.email))
               console.log('hola');
-          }})}},[])
+          }})}},[logueado])
 
 
 
