7fd6daa96f64b2d99d6d2e4c2f0310cc3e0ffce6
Ludwig Driet <83729472+LudwigDriet@users.noreply.github.com>
2022-01-13 00:21:34 -0300

    Siempre logeado (#134)
    * Update _formulario.scss
    
    * Update reducer.js
    
    * Update actions.js
    
    * Update ProfileHome.jsx
    
    * Update FormRegistro.jsx
    
    * Create ForLogin.jsx
    
    * Update App.js
    
    * Update FormRegistro.jsx
    
    * a

--- client/src/App.js
+++ client/src/App.js
@@ -18,15 +18,20 @@ import MyPage from './firebase/storage/MyPage';
 import MyAuthPage from './firebase/auth/MyAuthPage';
 import { useSelector, useDispatch } from 'react-redux';
 import { app } from "./firebase/firebase";
+import { getUserLogin } from './redux/actions';
+
+
 function App() {
-  const logueado = useSelector(state => state.userLogueado)
+  const logueado = useSelector(state => state.users)
   const dispatch = useDispatch()
 
   useEffect(() => {
 
     if (app) {
         app.auth().onAuthStateChanged((authUser) => {
-          if (authUser && !logueado.length) {
+          console.log('authUser',authUser.email)
+          if (authUser && logueado.length >4) {
+            dispatch(getUserLogin(authUser.email))
               console.log('hola');
           }})}},[])
 
