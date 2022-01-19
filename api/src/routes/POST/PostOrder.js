146593fb050460e4dc286322c3c7a97d794d0070
Analiadlt <87041305+Analiadlt@users.noreply.github.com>
2022-01-19 19:58:19 -0300

    Ruta orden compra (#183)
    Ruta Orden de compra - Ruta Mercado Pago - se agrega Id de Card a Ruta GetCard

---
+++ api/src/routes/POST/PostOrder.js
@@ -0,0 +1,41 @@
+const server = require('express').Router();
+const { Order, Card, User } = require("../../db");
+
+
+server.post('/', async (req, res, next) => {
+    const { email, allCards } = req.body 
+    try {
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+        
+        const order = await Order.create({
+            userId: user.id
+        })
+      
+        const cartas = await Card.findAll(
+            {
+                where: {
+                    id: allCards
+                }
+            }
+        )
+
+        if (!user) res.send('No existe el usuario.');
+        if (!cartas) res.send('No existe la carta solicitada.');
+
+        for (i = 0; i < cartas.length; i++) {
+            await order.addCards(cartas[i])
+        }
+         res.json(order);
+
+    } catch (error) {
+            res.send('Error en la operación')
+    }
+});
+
+
+
+module.exports = server;
