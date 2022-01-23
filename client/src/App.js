a53fa490d9c1bb7c13cdf37266627d98829daa06
Gustavo Montero <79276560+gamontero@users.noreply.github.com>
2022-01-23 18:01:28 -0300

    Login Metamask

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ import { getUserLogin } from "./redux/actions";
 import Comprar from "./components/compra/ordenCompra";
 import GanadorJuego from "./components/juego/GanadorJuego";
 import LoginMetamask from "./components/NFT/PaginaLoginMetamask.jsx";
-import { MoralisProvider } from "react-moralis";
+
 
 function App() {
   const logueado = useSelector((state) => state.users);
@@ -59,34 +59,29 @@ function App() {
   }, [dispatch, logueado]);
 
   return (
-    <MoralisProvider
-      appId="sF0LH5oGXTqeMld25GjGk8XEQAocbocN8D4XIlMK"
-      serverUrl="https://ltky0gpdxgky.usemoralis.com:2053/server"
-    >
-      <div className="App">
-        <Route path="/ganador" component={GanadorJuego} />
-        <Route path="/storage" component={MyPage} />
-        <Route path="/auth" component={MyAuthPage} />
-        <Route path="/Carrito" component={Carrito} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/Tienda" component={Tienda} />
-        <Route path="/Registro" component={FormRegistro} />
-        <Route path="/Login" component={ForLogin} />
-        <Route path="/PassReset" component={PassReset} />
-        <Route path="/Detail/" component={userDetail} />
-        <Route path="/chat" component={Chat} />
-        <Route path="/juego" component={NavCheto} />
-        <Route exact path="/TiendaNFT" component={TiendaNFT} />
-        <Route exact path="/CrearNFT" component={crearNFT} />
-        <Route exact path="/TableroNFT" component={tableroNFT} />
-        <Route exact path="/MisNFT" component={misNFT} />
-        <Route exact path="/Matchmaking" component={Intermedio} />
-        <Route exact path="/Compra" component={Compra} />
-        <Route exact path="/Chatear" component={ChatApp} />
-        <Route exact path="/Comprar" component={Comprar} />
-        <Route exact path="/LoginMetamask" component={LoginMetamask} />
-      </div>
-    </MoralisProvider>
+    <div className="App">
+      <Route path="/ganador" component={GanadorJuego} />
+      <Route path="/storage" component={MyPage} />
+      <Route path="/auth" component={MyAuthPage} />
+      <Route path="/Carrito" component={Carrito} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/Tienda" component={Tienda} />
+      <Route path="/Registro" component={FormRegistro} />
+      <Route path="/Login" component={ForLogin} />
+      <Route path="/PassReset" component={PassReset} />
+      <Route path="/Detail/" component={userDetail} />
+      <Route path="/chat" component={Chat} />
+      <Route path="/juego" component={NavCheto} />
+      <Route exact path="/TiendaNFT" component={TiendaNFT} />
+      <Route exact path="/CrearNFT" component={crearNFT} />
+      <Route exact path="/TableroNFT" component={tableroNFT} />
+      <Route exact path="/MisNFT" component={misNFT} />
+      <Route exact path="/Matchmaking" component={Intermedio} />
+      <Route exact path="/Compra" component={Compra} />
+      <Route exact path="/Chatear" component={ChatApp} />
+      <Route exact path="/Comprar" component={Comprar} />
+      <Route exact path="/LoginMetamask" component={LoginMetamask} />
+    </div>
   );
 }
 
