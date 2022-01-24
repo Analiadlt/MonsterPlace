fdd2d1bdf3d2eb0cc9601ef9c20143f1026e64de
Gustavo Montero <79276560+gamontero@users.noreply.github.com>
2022-01-24 02:14:55 -0300

    Boton Desconectar Wallet
    incorpore con los mismos estilos del panel el botón para desconectar el wallet. Adicionalmente agregué estado de conexión.

--- api/src/routes/PUT/PutMetamask.js
+++ api/src/routes/PUT/PutMetamask.js
@@ -1,25 +1,22 @@
-const {Router} = require ("express");
-const {User} = require("../../db");
-
-
+const { Router } = require("express");
+const { User } = require("../../db");
 const router = Router();
 
-router.put("/",async(req,res)=>{
+router.put("/", async (req, res) => {
+  const { metamaskAccount, email } = req.body;
+  try {
+    let response = await User.update(
+      { metamaskAccount },
+      {
+        where: {
+          email: email,
+        },
+      }
+    );
+    res.send(response);
+  } catch (error) {
+    res.send("Error al loguear usuario");
+  }
+});
 
-    const { metamaskAccount,email } = req.body
-   
-    try {
-        let response=await User.update({ metamaskAccount },
-            {
-            where: {
-                email:email,
-            },
-        });
-        res.send(response)
-    } catch (error) {
-        res.send("Error al loguear usuario")
-    }
-
-})
-
-module.exports = router
+module.exports = router;
