e4e305e3a2603e49ac417b38fc33aa1f58ea40e3
Gustavo Montero <79276560+gamontero@users.noreply.github.com>
2022-01-25 17:51:25 -0300

    Cambios de ruta
    ahroa vincula por email

--- api/src/routes/PUT/PutLinkUserCardNFT.js
+++ api/src/routes/PUT/PutLinkUserCardNFT.js
@@ -1,32 +1,24 @@
-const express = require("express")
-const { User, Card } = require("../../db")
-
-const server = express()
+const express = require("express");
+const { User, Card } = require("../../db");
+const server = express();
 
 server.put("/", async (req, res) => {
-    const { metamaskAccount, nftContract } = req.body
-
-    try {
-        const user = await User.findOne({ where: { metamaskAccount: metamaskAccount } })
-        const cards = await Card.findAll({ where: { nftContract: nftContract } })
-        console.log("user data base", user)
-        console.log("cards data base", cards)
-
-        if (!user) {
-            res.send("No existe el usuario")
-        }
-        if (!cards) {
-            res.send("No existe la carta solicitada")
-        }
-
-        user.addCards(cards);
-      
-        res.send("Carta agregadas satifactoriamente")
+  const { email, nftContract } = req.body;
+   try {
+    const user = await User.findOne({ where: { email: email }});
+    const cards = await Card.findAll({ where: { nftContract: nftContract }});
+    if (!user) {
+      res.status(400).send("No existe el usuario");
     }
-    catch (error) {
-        console.log(error)
-        res.send("Error en la base de datos")
+    if (!cards) {
+      res.status(400).send("No existe la carta solicitada");
+    } else if (user && cards) {
+      user.addCards(cards);
+      res.send("Carta agregadas satifactoriamente");
     }
-})
+  } catch (error) {
+    res.send("Error en la base de datos");
+  }
+});
 
-module.exports = server
+module.exports = server;
