e85077f7e56101d118cb8dbac58a9a7ae95b9080
Gustavo Montero <79276560+gamontero@users.noreply.github.com>
2022-01-24 21:54:30 -0300

    Action postCartaNFT

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -19,6 +19,7 @@ export const GET_PAGAR = "GET_PAGAR";
 export const RESTAR_SALDO = "RESTAR_SALDO";
 export const CARGAR_SALDO = "CARGAR_SALDO";
 export const ADD_METAMASK_ACCOUNT = "METAMASK_ACCOUNT";
+export const ADD_CARD_NFT = "ADD_CARD_NFT"
 
 
 
@@ -40,6 +41,21 @@ export function addUser(payload) {
     }
   };
 }
+
+export function postCardNFT(payload) {
+  return async (dispatch) => {
+    try {
+      var json = await axios.post('/postcards', payload);
+      return dispatch({
+        type: ADD_CARD_NFT,
+        payload: json,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+
 
 export function addMetamaskAccount(payload) {
   return async (dispatch) => {
