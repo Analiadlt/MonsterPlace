10f2f60c1a672b1c39d6e9d86b697c6b237ebac0
Analiadlt <87041305+Analiadlt@users.noreply.github.com>
2022-01-26 10:13:01 -0300

    Update GetCards.js

--- api/src/routes/GET/GetCards.js
+++ api/src/routes/GET/GetCards.js
@@ -153,13 +153,13 @@ router.get("/get", async (req, res) => {
   }  
   try {
     const cards1 = await Card.findAll({
-      attributes: ["id", "name", "attack", "defense", "img", "state", "type", "sellPrice", "orderId"],
+      //attributes: ["id", "name", "attack", "defense", "img", "state", "type", "sellPrice", "orderId"],
       order:[[ordenarpor, orden]]
     })
     if (cards1.length < 11) {
       CreateCards()
       const allCards = await Card.findAll({
-        attributes: ["id", "name", "attack", "defense", "img", "state", "type", "sellPrice", "orderId"],
+        //attributes: ["id", "name", "attack", "defense", "img", "state", "type", "sellPrice", "orderId"],
         order:[[ordenarpor, orden]]
       })
       res.status(200).send(allCards)
